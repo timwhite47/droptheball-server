08cbe814c0fa94a5237077b657394b194844edff
Tim White <tim@timwhite.ninja>
2018-06-09 13:15:29 -0700

    connect to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const mongodb = require("mongodb")
+var bodyParser = require("body-parser")
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+const PORT = process.env.PORT || 5000
+const PUBLIC_PATH = path.join(__dirname, 'public');
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/test";
+let db;
+
+// Initialize Server
+app = express()
+app.use(bodyParser.json());
+app.use(express.static(PUBLIC_PATH))
+
+// Views
+app.set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// Routes
+app.get('/', (req, res) => res.render('pages/index'))
+
+// Connect DB/Start server
+mongodb.MongoClient.connect(MONGO_URI, (err, client) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  let server = app.listen(PORT, function () {
+    let port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
